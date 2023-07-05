468ea98eae3c3e978deeb370d0c3a0aba9f3101c
Your NameVladimirs77nt <vladimirs77@yandex.ru>
2023-07-05 16:04:34 +0500

    Домашние задания к семинару №1 выполнены

--- Seminar_1.java
+++ Seminar_1.java
@@ -2,9 +2,7 @@ import java.io.IOException;
 import java.util.Random;
 import java.util.Scanner;
 
-/**
- * Program
- */
+
 public class Seminar_1 {
     static Scanner scanner = new Scanner(System.in);
     static Random random = new Random();
